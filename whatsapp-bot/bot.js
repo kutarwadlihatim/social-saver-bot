21714c8d62987e8ccd5efddbf96ed7d717e342ed
kutarwadlihatim <kutarwadliwalahatim01@gmail.com>
2026-02-22 15:57:03 +0530

    Update bot.js

--- whatsapp-bot/bot.js
+++ whatsapp-bot/bot.js
@@ -12,12 +12,11 @@ const client = new Client({
   }),
   puppeteer: {
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--disable-gpu"
+      "--disable-dev-shm-usage"
     ]
   }
 });
